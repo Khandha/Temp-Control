85e970feeb28c8b7e89bd261f0a4de03891c33dd
Khandha <wojciech.swiecicki1@gmail.com>
2021-12-26 21:52:26 +0100

    modified pid tunings

--- application/engine.py
+++ application/engine.py
@@ -1,4 +1,5 @@
 import time
+from multiprocessing import Process
 
 from PID import PID
 
@@ -29,20 +30,22 @@ class Engine:
         # setting pid controller
         # Cohen-Coon method
         # https://link.springer.com/article/10.1007/s42452-019-0929-y
-        pid = PID(3.9931, 0.4144, 2.6267, set_point=room1.expected_temp)
+        pid = PID(20, 0.0005, 20, set_point=room1.expected_temp)
 
         # setting pid limits
         pid.out_min, pid.out_max = self.power_min, self.power_max
 
         start_time = time.time()
         while True:
-            # TODO: split into process
             # pulls new temp from queue
-            if not q.empty():
-                new_temp = q.get()
-                pid.setpoint = new_temp
-                room1.expected_temp = new_temp
-                print("set new temp to: ", new_temp)
+            try:
+                if not q.empty():
+                    new_temp = q.get()
+                    pid.setpoint = new_temp
+                    room1.expected_temp = new_temp
+                    print("set new temp to: ", new_temp)
+            except ValueError:
+                print("Queue error - retrying in next run")
 
             # calculate power to heater
             power = pid(room1.room_temp)
@@ -53,9 +56,10 @@ class Engine:
             # returns room temperature based on differential equation
             room_temp = room1(heat, self.dt)
 
-            # TODO: split into process
             # saves state to database
-            db_push_temp(room_temp, heater_temp)
+            p1 = Process(target=db_push_temp, args=(room_temp, heater_temp))
+            p1.start()
+
             time.sleep(self.dt - ((time.time() - start_time) % self.dt))
 
     def time_prediction(self, room, queue):
@@ -63,7 +67,7 @@ class Engine:
         # increase dt to speed up the process
         self.dt *= speed_increase
         # set controller
-        pid = PID(1000, 0.001, 0.1, set_point=room.expected_temp)
+        pid = PID(20, 0.0005, 20, set_point=room.expected_temp)
         # set limits
         pid.out_min, pid.out_max = self.power_min, self.power_max
         # counter to estimate time
@@ -71,6 +75,7 @@ class Engine:
         heater_temps = []
         room_temps = []
         times = []
+        powers = []
         while True:
             i += 1
 
@@ -79,9 +84,6 @@ class Engine:
 
             # calculate heater temperature and heat given out
             heater_temp, heat = self.heater_temp_calculate(power, room.room_temp)
-
-            room_temp_last = room.room_temp
-
             # calculate room temperature based on heat given out
             room_temp = room(heat, self.dt)
 
@@ -89,14 +91,18 @@ class Engine:
             # START:
             room_temps.append(room_temp)
             heater_temps.append(heater_temp)
-            times.append(i * speed_increase)
-
+            times.append(i / speed_increase)
+            powers.append(power)
             # TODO: uncomment this:
-            # estimate if reached set temperature
-            # if round(room.room_temp, 1) and round(room_temp_last, 1) == room.expected_temp:
+            # check if reached set temperature
+            # if round(room.room_temp, 1) == room.expected_temp:
             #     break
+
             # TODO: remove this:
-            if i == 10000:
+            if i == 20000:
+                pid.set_point = 38
+                room.expected_temp = 38
+            if i == 40000:
                 break
             # END
 
@@ -106,9 +112,11 @@ class Engine:
         # Testing purposes only
         # START:
         output_file("C:/projectspy/Temp-Control/graph.html", title="estimated quick")
-        fig = figure(title="estimated", plot_height=750, plot_width=1350, x_axis_label="Time[s]", y_axis_label="temps[C]")
-        fig.line(times, room_temps, color="blue", legend_label="room temps[C]", line_width=3)
-        fig.line(times, heater_temps, color="red", legend_label="heater temps[C]", line_width=2)
+        fig = figure(title="estimated", plot_height=750, plot_width=1350,
+                     x_axis_label="Time[s]", y_axis_label="temps[C]")
+        fig.line(times, room_temps, color="blue", legend_label="room temp[C]", line_width=3)
+        fig.line(times, heater_temps, color="red", legend_label="heater temp[C]", line_width=2)
+        # fig.line(times, powers, color="green", legend_label="power[W]", line_width=1)
         show(fig)
         # END
 
@@ -121,7 +129,7 @@ class Engine:
             watts = 0
 
         # copper heat capacity: 0.385 kJ/kg, water heat capacity: 4.2 kJ/kg
-        cp = 0.5 * 4.2 + 0.5 * 0.385
+        cp = 0.9 * 4.2 + 0.1 * 0.385
 
         # mass of heater: 20 kg
         mass = 20
@@ -138,7 +146,7 @@ class Engine:
         surface = 0.57 * 0.42 * 2 + 0.19 * 0.42 * 2 + 0.19 * 0.57 * 2  # m^3
 
         # https://www.engineeringtoolbox.com/overall-heat-transfer-coefficient-d_434.html
-        coefficient = 12.5  # W/m^3K
+        coefficient = 12.5  # W/m^3K = J/s / m^3K
 
         # temperature difference
         temp_difference = room_temp - self.heater_temp_now  # C
@@ -175,7 +183,7 @@ class Room:
         #   mass of furniture = 100 kg
         #   cp of wood = 1.7 kJ/kg
         #   cp of air = 1.012 kJ/kg
-        #   B = 0.005
+        #   B = 0.0005
 
         mass = 100  # kg
         cp = 1.012 * 0.9 + 1.7 * 0.1  # kJ / kg
@@ -183,10 +191,9 @@ class Room:
         # Q = m * cp * dT  ===>  dT = Q / m * cp
         qin = heat / (mass * cp)
 
-        # to set up amount of heat to be put in
-        b = 0.001
-        # print("room temp: ", self.room_temp)
-        delta_temp = ((qin - b * pow(self.room_temp, 0.5)) * dt) / self.room_surface
-        # print("delta temp: ", delta_temp, "\n")
+        # heat loss coefficient
+        b = 0.0005
+
+        delta_temp = ((qin - b * pow(self.room_temp, 0.5)) * dt) / self.room_volume
         self.room_temp += delta_temp
         return self.room_temp
